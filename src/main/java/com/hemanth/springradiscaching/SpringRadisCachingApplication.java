9637d29d90af185cd93c54a26ca434e364f32d9e
hemanthcse1 <hemanthcse1@gmail.com>
2024-06-18 20:22:01 +0530

    initial commit

---
+++ src/main/java/com/hemanth/springradiscaching/SpringRadisCachingApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.springradiscaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRadisCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRadisCachingApplication.class, args);
+    }
+
+}
